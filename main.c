87ccfed8310815076419ddafec2a04f15db51c6c
matan06 <matan.garibi@gmail.com>
2026-01-10 19:35:52 +0200

    init project
    initilize project

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> 
+#include <sys/way.h>
+
+
+int int main(int argc, char *argv[])
+{ 
+  return EXIT_SUCCESS;
+}
